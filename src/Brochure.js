eac38f9e8d13bfc5bceb7ec452f98f3611f1ab5d
Autumn Raine <autumn.raine@icloud.com>
2018-02-23 11:41:18 -0700

    finished project

--- src/Brochure.js
+++ src/Brochure.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Sitenavbar from './Components/Sitenavbar'
-import './App.css';
+import './App.css'
+import './Brochure.css'
 
 const Brochure = () => {
   return(
-    <div>
+    <div className="App">
       <Sitenavbar />
-      <h1>Hello from Brochure</h1>
+      <h1>React Hacker News</h1>
+      <h3>What the App Does</h3>
+      <p>Using the <a href="https://github.com/HackerNews/API">Hacker News API</a> you can go to either Top, Best, or New Stories</p>
+      <p>App is built using ReactJS</p>
     </div>
   );
 }
